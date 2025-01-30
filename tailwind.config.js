cd87def6f93deb37d642b832f54fb87a9bd35634
Cinnamon Wolf <ag.skrip@gmail.com>
2025-01-29 23:29:10 -0800

    Moving away from PlayCDN to a proper Tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './assets/**/*.css',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
